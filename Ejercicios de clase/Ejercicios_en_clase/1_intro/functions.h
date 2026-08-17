44e16a750cd0292fa245e46aa8be0c82bd4b5eea
edjuarezp <10983425+ejuarezp@users.noreply.github.com>
2026-08-17 09:40:54 -0600

    funciones y recursión

--- Ejercicios de clase/Ejercicios_en_clase/1_intro/functions.h
+++ Ejercicios de clase/Ejercicios_en_clase/1_intro/functions.h
@@ -1,24 +1,54 @@
 /*
  * intro.h
+ *  Created on: 10/09/2015
  *      Author: pperezm
+ *  Mofified on: 17/08/2026
+ *      Author: edjuarezp
  */
 
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
 unsigned long fact(int n) {
-	return 0;
+	if (n <= 0) {
+		return 0;
+	}
+
+	unsigned long result = 1;
+	for (int i = 1; i <= n; i++) {
+		result = result * i;
+	}
+	return result;
 }
 
 bool isPrime(int n) {
-	return false;
+	if (n <= 1) {
+		return false;
+	}
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 unsigned long sum(int arr[], int size) {
-	return 0;
+	unsigned long result = 0;
+	for (int i = 0; i < size; i++) {
+		result = result + arr[i];
+	}
+	return result;
 }
 
 void reverse(int arr[], int size) {
+	int fin = size - 1;
+	for (int inicio = 0; inicio < size / 2; inicio++) {
+		int temp = arr[inicio];
+		arr[inicio] = arr[fin];
+		arr[fin] = temp;
+		fin--;
+	}
 }
 
 #endif /* FUNCTIONS_H_ */
--- Ejercicios de clase/Ejercicios_en_clase/2_recursion/recursion.h
+++ Ejercicios de clase/Ejercicios_en_clase/2_recursion/recursion.h
@@ -2,7 +2,7 @@
  * recursion.h
  *
  *  Created on: 10/09/2015
- *      Author: pperezm
+ *      Author: pperezm, edjuarezp
  */
 
 #ifndef RECURSION_H_
@@ -12,58 +12,109 @@
 #include <iostream>
 
 long sum_seq(int n) {
-	return 0;
+	long result = 0;
+	for (int i = 1; i <= n; i++) {
+		result = result + i;
+	}
+	return result;
 }
 
 long sum_rec(int n) {
-	return 0;
+	if (n <= 1) {
+		return 1;
+	}
+	return n + sum_rec(n - 1);
 }
 
 long fact_seq(int n) {
-	return 0;
+	long result = 1;
+	for (int i = 1; i <= n; i++) {
+		result = result * i;
+	}
+	return result;
 }
 
 long fact_rec(int n) {
-	return 0;
+	if (n <= 1) {
+		return 1;
+	}
+	return n * fact_rec(n - 1);
 }
 
 long fib_seq(int n) {
-	return 0;
+	if (n == 1) {
+		return 1;
+	}
+	if (n <= 0) {
+		return 0;
+	}
+
+	long result = 0;
+	long ant_1 = 1;
+	long ant_2 = 0;
+	for(int i = 2; i <= n; i++) {
+		result = ant_1 + ant_2;
+		ant_2 = ant_1;
+		ant_1 = result;
+	}
+	return result;
 }
 
 long fib_rec(int n) {
-	return 0;
+	if (n == 0) {
+		return 0;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	return fib_rec(n - 2) + fib_rec(n - 1);
 }
 
 long gcd_seq(long a, long b) {
-	return 0;
+	while (b != 0) {
+		long aux = b;
+		b = a % b;
+		a = aux;
+	}
+	return a;
 }
 
 long gcd_rec(long a, long b) {
-	return 0;
+	if (b == 0) {
+		return a;
+	}
+	return gcd_rec(b, a % b);
 }
 
 bool find_seq(int arr[], int size, int val) {
-	return false;
-}
-
-bool find_rec(int arr[], int low, int high, int val) {
-	return false;
-}
-
-int max_seq(int arr[], int size) {
+	
 	return 0;
 }
 
+bool find_rec(int arr[], int low, int high, int val) {
+	
+	
+	return 0;
+}
+
+int max_seq(int arr[], int size) { 
+	
+	return 0; 
+}
+
 int max_rec(int arr[], int low, int high) {
+	
+
 	return 0;
 }
 
 int unimodal_seq(int arr[], int size) {
+	
 	return 0;
 }
 
 int unimodal_rec(int arr[], int low, int high) {
+	
 	return 0;
 }
 
