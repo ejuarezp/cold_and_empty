dba8452c57b6368a4a1dca56286ee3f44046ca60
edjuarezp <10983425+ejuarezp@users.noreply.github.com>
2026-09-01 10:27:39 -0600

    algoritmos de búsqueda

--- Ejercicios de clase/Ejercicios_en_clase/3_busquedas/busquedas.h
+++ Ejercicios de clase/Ejercicios_en_clase/3_busquedas/busquedas.h
@@ -2,7 +2,7 @@
  * recursion.h
  *
  *  Created on: 10/09/2015
- *      Author: pperezm
+ *      Author: pperezm, edjuarezp
  */
 
 #ifndef BUSQUEDAS_H_
@@ -13,14 +13,41 @@
 
 
 int bs_seq(int arr[], int size, int val) {
-	return 0;
+	
+	int inicio = 0;
+	int fin = size - 1;
+	int mitad = (inicio + fin) / 2;
+	while (inicio < fin) {
+		if (arr[mitad] == val) {
+			return mitad;
+		}
+
+		if (val < arr[mitad]) {
+			fin = mitad - 1;
+		} else {
+			inicio = mitad + 1;
+		}
+		mitad = (inicio + fin) / 2;
+	}
+
+	return mitad;
 }
 
 int bs_aux(int arr[], int low, int high, int val) {
-	return 0;
+	int mitad = (low + high) / 2;
+
+	if (low > high) {
+		return low;
+	} else	if (arr[mitad] == val) {
+		return mitad;
+	} else if (val < arr[mitad]) {
+		return bs_aux(arr, low, mitad - 1, val);
+	} else {
+		return bs_aux(arr, mitad + 1, high, val);
+	}
 }
 
 int bs_rec(int arr[], int size, int val) {
-	return 0;
+	return bs_aux(arr, 0, size - 1, val);
 }
 #endif
